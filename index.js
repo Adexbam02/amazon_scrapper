2dc4775ae64a30b7b87ded2bf9baa532e43d3e1c
Nurain <nurainbamidelejnr@gmail.com>
2023-09-20 01:44:25 +0100

    reviews, offers, search added

--- index.js
+++ index.js
@@ -29,6 +29,69 @@ app.get("/products/:productId", async (req, res) => {
   }
 });
 
+// Get product Reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  //   const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+// Get product Reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  //   const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get product Offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  //   const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get product Search Result
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  //   const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
